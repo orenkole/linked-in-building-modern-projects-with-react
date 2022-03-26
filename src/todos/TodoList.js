a54657a1b06814acbfdd2b532922c5c06fd49516
badger <badger@badger.local>
2022-03-26 19:28:54 +0200

    Adding another reducer

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,32 +1,42 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {connect} from "react-redux";
 import TodoListItem from "./TodoListItem.js";
 import NewTodoForm from "./NewTodoForm.js";
 import "./TodoList.css";
 import { removeTodo } from "./actions.js";
 import { markTodoAsCompleted } from "./actions.js";
-import {displayAlert} from "./thunks";
+import {loadTodos} from "./thunks";
 
 const TodoList = ({
 	todos = [],
 	onRemovePressed,
 	onCompletedPressed,
+	isLoading,
+	startLoadingTodos
 }) => {
-	return (
+	useEffect(() => {
+		startLoadingTodos
+	}, [])
+	const loadingMessage = <div>Loading todos...</div>
+	const content = (
 		<div className="list-wrapper">
 			<NewTodoForm />
 			{todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
 		</div>
 	)
+
+	return isLoading ? loadingMessage : content;
 }
 
 const mapStateToProps = state => ({
 	todos: state.todos,
+	isLoading: state.isLoading,
 })
 
 const mapDispatchToProps = dispatch => ({
 	onRemovePressed: text => dispatch(removeTodo(text)),
 	onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+	startLoadingTodos: () => dispatch(loadTodos())
 })
 
 export default connect(
--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,4 +1,28 @@
-import { CREATE_TODO, MARK_TODO_AS_COMPLETED, REMOVE_TODO } from "./actions";
+import {
+	CREATE_TODO,
+	LOAD_TODOS_FAILURE,
+	LOAD_TODOS_IN_PROGRESS,
+	LOAD_TODOS_SUCCESS,
+	MARK_TODO_AS_COMPLETED,
+	REMOVE_TODO
+} from "./actions";
+
+export const isLoading = (state = false, action) => {
+	const {type} = action;
+	switch(type) {
+		case LOAD_TODOS_IN_PROGRESS: {
+			return true
+		}
+		case LOAD_TODOS_SUCCESS: {
+			return false
+		}
+		case LOAD_TODOS_FAILURE: {
+			return false
+		}
+		default:
+			return state;
+	}
+}
 
 export const todos = (state = [], action) => {
 	const {type, payload} = action;
