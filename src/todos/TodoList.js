9dcc22d1b986b555fdc1a74f377a89421703aa64
badger <badger@badger.local>
2022-03-26 18:06:07 +0200

    Adding a redux flow

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -3,13 +3,14 @@ import {connect} from "react-redux";
 import TodoListItem from "./TodoListItem.js";
 import NewTodoForm from "./NewTodoForm.js";
 import "./TodoList.css";
-import { removeTodo } from "../actions.js";
+import { removeTodo } from "./actions.js";
+import { markTodoAsCompleted } from "./actions.js";
 
-const TodoList = ({todos = [], onRemovePressed}) => {
+const TodoList = ({todos = [], onRemovePressed, onCompletedPressed}) => {
 	return (
 		<div className="list-wrapper">
 			<NewTodoForm />
-			{todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />)}
+			{todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
 		</div>
 	)
 }
@@ -19,7 +20,8 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-	onRemovePressed: text => dispatch(removeTodo(text))
+	onRemovePressed: text => dispatch(removeTodo(text)),
+	onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
 })
 
 export default connect(
--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./TodoListItem.css";
 
-const TodoListItem = ({todo, onRemovePressed}) => {
+const TodoListItem = ({todo, onRemovePressed, onCompletedPressed}) => {
 	return (
 		<div className="todo-item-container">
 			<h3>{todo.text}</h3>
 			<div className="buttons-container">
-				<button className="completed-button">Mark as completed</button>
+				{!todo.isCompleted && (
+					<button className="completed-button"
+					onClick={() => onCompletedPressed(todo.text)}
+				>Mark as completed</button>
+				)}
 				<button className="remove-button"
 					onClick={() => onRemovePressed(todo.text)}
 				>remove</button>
