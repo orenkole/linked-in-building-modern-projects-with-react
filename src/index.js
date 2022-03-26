446f20cab9075b8d6ced10131a50ce25f14794a9
badger <badger@badger.local>
2022-03-26 17:34:03 +0200

    Persisting the redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/lib/integration/react";
 import {Provider} from "react-redux";
 import {configureStore} from "./store.js";
 import App from "./App.js";
 import {hot} from "react-hot-loader/root";
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-	<Provider store={configureStore()} >
-		<App />
+	<Provider store={store} >
+		<PersistGate
+			persistor={persistor }
+			loading={<div>Loading...</div>}
+		>
+			<App />
+		</PersistGate>
 	</Provider>,
 	document.getElementById("root")
 )
