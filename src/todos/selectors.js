42f5c45ea93ede1ca0387061eeb6dbb215e48d1a
badger <badger@badger.local>
2022-03-27 07:40:56 +0300

    Combining selectors with reselect

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -1,2 +1,18 @@
+import { createSelector } from "reselect";
+
 export const getTodos = state => state.todos.data;
 export const getTodosLoading = state => state.todos.isLoading;
+
+export const getIncompleteTodos = createSelector(
+	getTodos,
+	(todos) => todos.filter(todo => !todo.isCompleted),
+)
+
+// example with 2 selectors:
+// export const getIncompleteTodos = createSelector(
+// 	getTodos,
+// 	getTodosLoading,
+// 	(todos, isLoading) => isLoading
+// 	? []
+// 	: todos.filter(todo => !todo.isCompleted),
+// )
